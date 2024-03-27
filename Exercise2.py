2d0a61945b55e7e48bf2a0dc5db8b26ec44c0719
PharoahEgbuna <pharoahegbuna@gmail.com>
2024-03-27 18:54:11 -0400

    Create Exercise2.py

---
+++ Exercise2.py
@@ -0,0 +1,7 @@
+# My first comment
+# Anything after the # (hashtag) is ignored by python
+
+print("I could print a line like this.") #and comment after
+
+#print("This wont run.")
+print("This will run.")
