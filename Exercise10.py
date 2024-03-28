f03f0c22716bcdff2282e8603ffc1ef9dfc5470f
PharoahEgbuna <pharoahegbuna@gmail.com>
2024-03-28 17:21:12 -0400

    Added exercises 8-10

---
+++ Exercise10.py
@@ -0,0 +1,17 @@
+# \t is an escape sequence that causes the string to print indented or tabbed
+# \\ is an escape sequence which allow a backslash to appear in the string
+tabby_cat = "\tI'm tabbed in"
+persian_cat = "I'm split\non a line."
+backslash_cat = "I'm \\ a \\ cat"
+
+fat_cat = '''
+I'll do a list:
+\t* Cat food
+\t* Fishes
+\t* Catnip\n\t* Grass 
+'''
+
+print(tabby_cat)
+print(persian_cat)
+print(backslash_cat)
+print(fat_cat)
