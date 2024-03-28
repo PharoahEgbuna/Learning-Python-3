2f7a547f8c9b170fa46d76b4a9bbecca26665ab8
PharoahEgbuna <pharoahegbuna@gmail.com>
2024-03-28 17:26:08 -0400

    Added Exercise 11

---
+++ Exercise11.py
@@ -0,0 +1,11 @@
+# input() is used to receive responses from the user. By defauly input() returns a string 
+print("How old are you?", end = " ")
+age = input()
+
+print("How tall are you?", end = " ")
+height = input()
+
+print("how much do you weigh?", end = ' ')
+weight = input()
+
+print(f"So, you are {age} old, {height} tall and wiegh {weight}.")
