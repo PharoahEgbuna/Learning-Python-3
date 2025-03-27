5af2a2ad0399203c12f0ea2d5e006dba06f59955
Pharoah Egbuna <pharoahegbuna@gmail.com>
2025-03-27 18:19:02 +0000

    Create Unpacking_Parameters.py

---
+++ Unpacking_Parameters.py
@@ -0,0 +1,8 @@
+from sys import argv
+#read theWYSSsectionforhowtorunthis
+script, first, second, third = argv
+
+print("Thescriptiscalled:", script)
+print("Yourfirstvariableis:", first)
+print("Yoursecondvariableis:", second)
+print("Yourthirdvariableis:", third)
